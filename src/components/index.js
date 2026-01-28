df85f14b14e92f3a5aaf1081a21bc0841f4fc3de
breverdbidder <brevardbidderai@gmail.com>
2026-01-28 13:43:32 -0500

    refactor(P1): Add component index exports

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+/**
+ * SPD.AI Component Index
+ * Export all components for easy importing
+ * 
+ * @author BidDeed.AI / Everest Capital USA
+ */
+
+export { default as ChatInterface } from './ChatInterface';
+export { default as FormInterface } from './FormInterface';
+export { default as Results } from './Results';
+export { default as MapView } from './MapView';
+
+export * from './constants';
